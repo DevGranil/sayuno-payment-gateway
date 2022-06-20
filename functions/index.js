71149d16afc4412d49f95a8a07ac7dc4ded9223c
michael <mike@peaccce.com>
2022-06-20 09:01:32 +0100

    stripecheckout v1

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+const stripe = require("stripe")(functions.config().stripe.secret_key)
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.stripeCheckout = functions.https.onCall(async(data, context) => {
+
+    var db = admin.firestore();
+    var productId = data['id'];
+    console.log(productId)
+    var doc = await db.collection('price-plans').doc(productId).get();
+   
+    var price = doc.data().price;
+    var name = doc.data().name;
+   
+ 
+
+
+    const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [{
+            name: name,
+            description: 'description',
+            amount: Math.round(price * 100),
+            currency: 'gbp',
+            quantity: 1
+
+        }],
+        mode: 'payment',
+        success_url: 'http://localhost:4200/app/plans?action=success',
+        cancel_url: 'http://localhost:4200/app/plans?action=cancel'
+    })
+
+    return session.id;
+})
